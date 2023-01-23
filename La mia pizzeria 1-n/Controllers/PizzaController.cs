a8a59a9790d047426bf35085648a4e47e986d8b7
fusellifrancesco <fusellifrancesco05@gmail.com>
2023-01-23 19:01:59 +0100

    Modificati metodi Details, Create e Update del controller per aggiunta relazione n-n

--- La mia pizzeria 1-n/Controllers/PizzaController.cs
+++ La mia pizzeria 1-n/Controllers/PizzaController.cs
@@ -1,8 +1,9 @@
 ﻿using La_mia_pizzeria_1_n.Database;
 using Microsoft.AspNetCore.Mvc;
 using La_mia_pizzeria_1_n.Models;
-using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
+using La_mia_pizzeria_1_n.Utils;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace La_mia_pizzeria_1_n.Controllers {
     public class PizzaController : Controller {
@@ -25,6 +26,7 @@ namespace La_mia_pizzeria_1_n.Controllers {
                 Pizza PizzaTrovata = db.Pizze
                     .Where(SingolaPizzaNelDb => SingolaPizzaNelDb.Id == id)
                     .Include(pizza => pizza.Category)
+                    .Include(pizza => pizza.Tags)
                     .FirstOrDefault();
 
                 if (PizzaTrovata != null) {
@@ -40,12 +42,14 @@ namespace La_mia_pizzeria_1_n.Controllers {
         public IActionResult Create() {
 
             using (PizzaContext db = new PizzaContext()) {
+                
                 List<Category> categoriesFromDb = db.Categories.ToList<Category>();
 
                 PizzaCategoriesView modelForView = new PizzaCategoriesView();
                 modelForView.Pizza = new Pizza();
 
                 modelForView.Categories = categoriesFromDb;
+                modelForView.Tags = TagsConverter.getListTagsForMultipleSelect();
 
                 return View("Create", modelForView);
             }
@@ -59,6 +63,8 @@ namespace La_mia_pizzeria_1_n.Controllers {
                     List<Category> categories = db.Categories.ToList<Category>();
 
                     formData.Categories = categories;
+
+                    formData.Tags = TagsConverter.getListTagsForMultipleSelect();
                 }
 
 
@@ -66,27 +72,58 @@ namespace La_mia_pizzeria_1_n.Controllers {
             }
 
             using (PizzaContext db = new PizzaContext()) {
+                if (formData.TagsSelectedFromMultipleSelect != null) {
+                    formData.Pizza.Tags = new List<Tag>();
+
+                    foreach (string tagId in formData.TagsSelectedFromMultipleSelect) {
+                        int tagIdIntFromSelect = int.Parse(tagId);
+
+                        Tag tag = db.Tags.Where(tagDb => tagDb.Id == tagIdIntFromSelect).FirstOrDefault();
+
+                        // todo controllare eventuali altri errori tipo l'id del tag non esiste
+
+                        formData.Pizza.Tags.Add(tag);
+                    }
+                }
+
                 db.Pizze.Add(formData.Pizza);
                 db.SaveChanges();
             }
 
             return RedirectToAction("Index");
         }
+    
 
         [HttpGet]
         public IActionResult Update(int id) {
             using (PizzaContext db = new PizzaContext()) {
-                Pizza postToUpdate = db.Pizze.Where(pizza => pizza.Id == id).FirstOrDefault();
+                Pizza pizzaToUpdate = db.Pizze.Where(pizza => pizza.Id == id).Include(pizza => pizza.Tags).FirstOrDefault();
 
-                if (postToUpdate == null) {
+                if (pizzaToUpdate == null) {
                     return NotFound("La pizza non è stata trovata");
                 }
 
                 List<Category> categories = db.Categories.ToList<Category>();
 
                 PizzaCategoriesView modelForView = new PizzaCategoriesView();
-                modelForView.Pizza = postToUpdate;
+                modelForView.Pizza = pizzaToUpdate;
                 modelForView.Categories = categories;
+
+
+                List<Tag> listTagFromDb = db.Tags.ToList<Tag>();
+
+                List<SelectListItem> listaOpzioniPerLaSelect = new List<SelectListItem>();
+
+                foreach (Tag tag in listTagFromDb) {
+                    // Ricerco se il tag che sto inserindo nella lista delle opzioni della select era già stato selezionato dall'utente
+                    // all'interno della lista dei tag del post da modificare
+                    bool eraStatoSelezionato = pizzaToUpdate.Tags.Any(tagSelezionati => tagSelezionati.Id == tag.Id);
+
+                    SelectListItem opzioneSingolaSelect = new SelectListItem() { Text = tag.Title, Value = tag.Id.ToString(), Selected = eraStatoSelezionato };
+                    listaOpzioniPerLaSelect.Add(opzioneSingolaSelect);
+                }
+
+                modelForView.Tags = listaOpzioniPerLaSelect;
 
                 return View("Update", modelForView);
             }
@@ -108,7 +145,7 @@ namespace La_mia_pizzeria_1_n.Controllers {
             }
 
             using (PizzaContext db = new PizzaContext()) {
-                Pizza pizzaToUpdate = db.Pizze.Where(pizza => pizza.Id == id).FirstOrDefault();
+                Pizza pizzaToUpdate = db.Pizze.Where(pizza => pizza.Id == id).Include(pizza => pizza.Tags).FirstOrDefault();
 
                 if (pizzaToUpdate != null) {
 
@@ -117,11 +154,27 @@ namespace La_mia_pizzeria_1_n.Controllers {
                     pizzaToUpdate.Img = formData.Pizza.Img;
                     pizzaToUpdate.CategoryId = formData.Pizza.CategoryId;
 
+                    // rimuoviamo i tag e inseriamo i nuovi
+                    pizzaToUpdate.Tags.Clear();
+
+                    if (formData.TagsSelectedFromMultipleSelect != null) {
+
+                        foreach (string tagId in formData.TagsSelectedFromMultipleSelect) {
+                            int tagIdIntFromSelect = int.Parse(tagId);
+
+                            Tag tag = db.Tags.Where(tagDb => tagDb.Id == tagIdIntFromSelect).FirstOrDefault();
+
+                            // todo controllare eventuali altri errori tipo l'id del tag non esiste
+
+                            pizzaToUpdate.Tags.Add(tag);
+                        }
+                    }
+
                     db.SaveChanges();
 
                     return RedirectToAction("Index");
                 } else {
-                    return NotFound("La pizza che volevi modificare non è stata trovata!");
+                    return NotFound("Il post che volevi modificare non è stato trovato!");
                 }
             }
 
