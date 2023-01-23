31102fcd42d2ed1757515cea579f28361442859c
fusellifrancesco <fusellifrancesco05@gmail.com>
2023-01-23 17:18:00 +0100

    creato e completato model PizzaCategoriesView

---
+++ La mia pizzeria 1-n/Models/PizzaCategoriesView.cs
@@ -0,0 +1,8 @@
+﻿namespace La_mia_pizzeria_1_n.Models {
+    public class PizzaCategoriesView {
+        
+        public Pizza Pizza { get; set; }
+
+        public List<Category>? Categories { get; set; }
+    }
+}
