66d6b3b2e8fa80600d00bf174b4a26d7e281d72d
fusellifrancesco <fusellifrancesco05@gmail.com>
2023-01-24 17:48:38 +0100

    estensione Context con IdentityDbContext ed eseguita migration

--- La mia pizzeria 1-n/Database/PizzaContext.cs
+++ La mia pizzeria 1-n/Database/PizzaContext.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using La_mia_pizzeria_1_n.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace La_mia_pizzeria_1_n.Database {
-    public class PizzaContext : DbContext {
+    public class PizzaContext : IdentityDbContext<IdentityUser> {
 
         public DbSet<Pizza> Pizze { get; set; }
 
